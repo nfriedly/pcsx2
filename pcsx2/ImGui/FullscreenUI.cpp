2dbe64610eb482c8dc83ac008865cd5d3b2905d4
Nathan Friedly <nathan@nfriedly.com>
2026-04-10 23:02:31 -0400

    FSUI: Add Change Disc submenu with .m3u entries when present
    If a .m3u file was opened, Change Disc now opens a sub menu with the entries from the playlist and an option to open the file browser. If there wasn't a .m3u playlist, it just opens the file browser directly.

--- pcsx2/ImGui/FullscreenUI.cpp
+++ pcsx2/ImGui/FullscreenUI.cpp
@@ -964,9 +964,16 @@ void FullscreenUI::RequestChangeDisc()
 {
 	ConfirmShutdownIfMemcardBusy([](bool result) {
 		if (result)
-			DoChangeDiscFromFile();
+		{
+			if (!VMManager::GetM3UPlaylistEntries().empty())
+				OpenPauseSubMenu(PauseSubMenu::ChangeDisc);
+			else
+				DoChangeDiscFromFile();
+		}
 		else
+		{
 			ClosePauseMenu();
+		}
 	});
 }
 
@@ -1668,6 +1675,10 @@ void FullscreenUI::DrawPauseMenu(MainWindowType type)
 						first_id = ImGui::GetID(FSUI_ICONSTR(ICON_PF_BACKWARD, "Back To Pause Menu"));
 						last_id = ImGui::GetID(FSUI_ICONSTR(ICON_FA_STOPWATCH, "Leaderboards"));
 						break;
+					case PauseSubMenu::ChangeDisc:
+						first_id = ImGui::GetID(FSUI_ICONSTR(ICON_PF_BACKWARD, "Back To Pause Menu"));
+						last_id = ImGui::GetID(FSUI_ICONSTR(ICON_FA_FOLDER_OPEN, "Select Disc Image"));
+						break;
 				}
 
 				if (first_id != 0 && last_id != 0)
@@ -1796,6 +1807,35 @@ void FullscreenUI::DrawPauseMenu(MainWindowType type)
 
 				if (ActiveButton(FSUI_ICONSTR(ICON_FA_STOPWATCH, "Leaderboards"), false))
 					OpenLeaderboardsWindow();
+			}
+			break;
+
+
+			case PauseSubMenu::ChangeDisc:
+			{
+				if (just_focused)
+					ImGui::SetFocusID(ImGui::GetID(FSUI_ICONSTR(ICON_PF_BACKWARD, "Back To Pause Menu")), ImGui::GetCurrentWindow());
+
+				if (ActiveButton(FSUI_ICONSTR(ICON_PF_BACKWARD, "Back To Pause Menu"), false) || WantsToCloseMenu())
+					OpenPauseSubMenu(PauseSubMenu::None);
+
+				const std::vector<std::string>& playlist = VMManager::GetM3UPlaylistEntries();
+				const int active_index = VMManager::GetM3UPlaylistCurrentIndex();
+				for (int i = 0; i < static_cast<int>(playlist.size()); ++i)
+				{
+					const std::string label = fmt::format("{}: {}", i + 1, Path::GetFileName(playlist[i]));
+					const bool is_active = (i == active_index);
+					if (ActiveButton(FSUI_ICONSTR(ICON_FA_COMPACT_DISC, label.c_str()), is_active))
+					{
+						Host::RunOnCPUThread([path = playlist[i]]() { VMManager::ChangeDisc(CDVD_SourceType::Iso, path); });
+						ClosePauseMenu();
+					}
+				}
+
+				if (ActiveButton(FSUI_ICONSTR(ICON_FA_FOLDER_OPEN, "From File..."), false))
+				{
+					DoChangeDiscFromFile();
+				}
 			}
 			break;
 		}
